e1a33cb727cc5cd1b003637cc0a9a0bf5831e995
Home PC <example@mail.com>
2024-07-08 19:21:34 +0300

    Добавлены файлы

---
+++ src/main/java/org/example/creditscoreservice/CreditscoreserviceApplication.java
@@ -0,0 +1,13 @@
+package org.example.creditscoreservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreditscoreserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CreditscoreserviceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/creditscoreservice/CreditscoreserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.creditscoreservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreditscoreserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
